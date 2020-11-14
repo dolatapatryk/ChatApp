196d6317089a7adbc63291a47dd6e7c8ee146a03
dolatapatryk1 <dolata.patryk1@gmail.com>
2020-11-14 02:11:11 +0100

    implemented login logic

--- app/src/main/java/pl/patrykdolata/chatapp/activities/LoginActivity.kt
+++ app/src/main/java/pl/patrykdolata/chatapp/activities/LoginActivity.kt
@@ -1,28 +1,28 @@
 package pl.patrykdolata.chatapp.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.widget.Toast
-import com.google.firebase.FirebaseApp
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import kotlinx.android.synthetic.main.login_activity.*
 import pl.patrykdolata.chatapp.R
 import pl.patrykdolata.chatapp.services.SocketService
+import pl.patrykdolata.chatapp.utils.StringUtils
 
 class LoginActivity : AppCompatActivity() {
 
-//    private lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
-//        auth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
 
         textInputEditTextEmail.setText("dolata.patryk1@gmail.com")
         textInputEditTextPassword.setText("patryk")
@@ -46,28 +46,40 @@ class LoginActivity : AppCompatActivity() {
         SocketService.disconnect()
     }
 
-    private fun goToRegister() {
-        val intent = Intent(this, RegisterActivity::class.java)
-        changeIntent(intent)
-    }
-
     private fun login() {
+        progressBar.visibility = View.VISIBLE
         val email = textInputEditTextEmail.text.toString().trim()
         val password = textInputEditTextPassword.text.toString()
 
-//        auth.signInWithEmailAndPassword(email, password)
-//            .addOnCompleteListener { task ->
-//                if(task.isSuccessful) {
-//                    Toast.makeText(this, "login success", Toast.LENGTH_LONG)
-//                        .show()
-//                    goToMainActivity()
-//                } else {
-//                    println(task.exception)
-//                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG)
-//                        .show()
-//                }
-//            }
-        goToMainActivity()
+        if (isFormValid(email, password)) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Pomyślne logowanie!", Toast.LENGTH_LONG)
+                            .show()
+                        goToMainActivity()
+                    } else {
+                        onFailedLogin(task.exception)
+                    }
+                    progressBar.visibility = View.GONE
+                }
+        } else {
+            progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun onFailedLogin(exception: Exception?) {
+        val errorMessage = when (exception) {
+            is FirebaseAuthInvalidUserException, is FirebaseAuthInvalidCredentialsException ->
+                "Wprowadzono nieprawidłowy adres email lub hasło!"
+            else -> "Logowanie nie powiodło się!"
+        }
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+    }
+
+    private fun goToRegister() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        changeIntent(intent)
     }
 
     private fun goToMainActivity() {
@@ -77,8 +89,29 @@ class LoginActivity : AppCompatActivity() {
 
     private fun changeIntent(intent: Intent) {
         Handler().postDelayed(
-            {startActivity(intent)},
+            {
+                startActivity(intent)
+                clearForm()
+            },
             10
         )
+    }
+
+    private fun isFormValid(email: String, password: String): Boolean {
+        return if (!checkField(email, "Wprowadź adres email!")) {
+            false
+        } else checkField(password, "Wprowadź hasło!")
+    }
+
+    private fun checkField(field: String, errorMessage: String): Boolean {
+        return if (StringUtils.isEmpty(field)) {
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            false
+        } else true
+    }
+
+    private fun clearForm() {
+        textInputEditTextEmail.setText("")
+        textInputEditTextPassword.setText("")
     }
 }
