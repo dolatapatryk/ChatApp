9cd4e628b55c3eb8dc2ad466cbb00c14da0e634c
dolatapatryk1 <dolata.patryk1@gmail.com>
2020-11-15 14:26:38 +0100

    implemented searching for friends

--- app/src/main/java/pl/patrykdolata/chatapp/fragments/FriendsFragment.kt
+++ app/src/main/java/pl/patrykdolata/chatapp/fragments/FriendsFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.friends_fragment.*
 import kotlinx.android.synthetic.main.friends_fragment.view.*
 import pl.patrykdolata.chatapp.R
 import pl.patrykdolata.chatapp.adapters.FriendsAdapter
@@ -24,6 +27,7 @@ class FriendsFragment : Fragment() {
     private lateinit var friendsRecyclerView: RecyclerView
     private lateinit var searchRecyclerView: RecyclerView
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +36,6 @@ class FriendsFragment : Fragment() {
     ): View? {
         val view: View = inflater.inflate(R.layout.friends_fragment, container, false)
         initFieldsFromView(view)
-
-        progressBar.visibility = View.GONE
 
         pendingRecyclerView.layoutManager = LinearLayoutManager(activity)
         friendsRecyclerView.layoutManager = LinearLayoutManager(activity)
@@ -47,20 +49,36 @@ class FriendsFragment : Fragment() {
         friendsRecyclerView.adapter = friendsAdapter
         searchRecyclerView.adapter = searchFriendsAdapter
 
-        SocketService.on(SocketConstants.GET_USERS_RESPONSE) {args ->
-            onGetUsersResponse(args)
-        }
-        SocketService.emit(SocketConstants.GET_USERS)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val userId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+                val searchValue: String = query ?: ""
+                SocketService.on(SocketConstants.FIND_USERS_RESPONSE_EVENT) { args ->
+                    onFindUsersResponse(args)
+                }
+                SocketService.emit(SocketConstants.FIND_USERS_EVENT, userId, searchValue)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
 
         swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary)
 
         return view
     }
 
-    private fun onGetUsersResponse(responseArgs: Array<Any>) {
+    private fun onFindUsersResponse(responseArgs: Array<Any>) {
+        SocketService.off(SocketConstants.FIND_USERS_RESPONSE_EVENT)
         activity?.runOnUiThread {
             val userList: List<Friend> = JsonUtils.fromJsonArray(responseArgs[0] as String)
             searchRecyclerView.adapter = FriendsAdapter(userList)
+            noSearchResultTextView.visibility = when (userList.isEmpty()) {
+                true -> View.VISIBLE
+                false -> View.GONE
+            }
         }
     }
 
@@ -70,5 +88,6 @@ class FriendsFragment : Fragment() {
         friendsRecyclerView = view.friendsRecyclerView
         searchRecyclerView = view.searchRecyclerView
         swipeRefreshLayout = view.swipeRefreshLayout
+        searchView = view.searchView
     }
 }
--- app/src/main/java/pl/patrykdolata/chatapp/utils/SocketConstants.kt
+++ app/src/main/java/pl/patrykdolata/chatapp/utils/SocketConstants.kt
@@ -4,6 +4,8 @@ object SocketConstants {
     const val REGISTER_EVENT = "register"
     const val CHECK_USERNAME_EVENT = "check_username"
     const val CHECK_USERNAME_RESPONSE_EVENT = "check_username_response"
-    const val GET_USERS = "get_users"
-    const val GET_USERS_RESPONSE = "get_users_response"
+    const val GET_USERS_EVENT = "get_users"
+    const val GET_USERS_RESPONSE_EVENT = "get_users_response"
+    const val FIND_USERS_EVENT = "find_users"
+    const val FIND_USERS_RESPONSE_EVENT = "find_users_response"
 }
